3ae9e5ae1c9992b45a575fbf20e57016ee31a78d
Raimon <ramonkoudsi@gmail.com>
2020-02-10 04:27:44 -0400

    cambios

--- Node-FirstWeb/src/index.js
+++ Node-FirstWeb/src/index.js
@@ -27,7 +27,7 @@ app.set('view engine', 'ejs');
 app.use(require('./routes/index'));
 
 //archivos estaticos
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 //escuchando el servidor
 app.listen(app.get('port'), () => 
